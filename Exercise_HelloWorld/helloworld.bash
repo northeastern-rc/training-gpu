2b0f61ff2cd295006ed84e005f5d4a6cae7d7a5c
Manasvita Joshi <github@manasvita.com>
2022-05-23 15:36:32 -0400

    script for the HelloWorld example

---
+++ Exercise_HelloWorld/helloworld.bash
@@ -0,0 +1,20 @@
+#!/bin/bash 
+#SBATCH --job-name=Hello
+#SBATCH --nodes=1  
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:20:00
+#SBATCH --gres=gpu:v100-sxm2:1    ##request 1 V100 gpu
+## Inside training - use the training reservation:
+#SBATCH --partition=reservation
+#SBATCH --reservation=Training-GPU
+## Outside of training - uncomment the line below and delete the above two lines:
+##SBATCH --partition=gpu
+
+## Load the CUDA module to access nvcc compiler and CUDA libraires:
+module load cuda/11.1
+
+## compile the c+cuda code using the nvcc compiler. Create the 'helloworld' executable program:
+nvcc helloworld.cu -o helloworld
+
+## Run the executable program:
+./helloworld
