5fa4b143301a183fdfca492d66ea4c4e1d873f3c
Joseph Cooke <j.cooke@northeastern.edu>
2023-06-09 16:27:56 -0400

    Added in bootcamp reservation information

--- Exercise_3_TF_sbatch/main_tf.bash
+++ Exercise_3_TF_sbatch/main_tf.bash
@@ -1,10 +1,20 @@
 #!/bin/bash
+
+#####Use after training##############
+##SBATCH --partition=gpu
+##SBATCH --gres=gpu:p100:1
+#####################################
+
+#####Use during training###############
+#SBATCH --partition=reservation
+#SBATCH --reservation=bootcamp_gpu_2023
+#SBATCH --gres=gpu:1
+#######################################
+
 #SBATCH --job-name=TFmult
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=00:30:00
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:p100:1
 
 module load anaconda3/2021.05
 module load cuda/11.1
